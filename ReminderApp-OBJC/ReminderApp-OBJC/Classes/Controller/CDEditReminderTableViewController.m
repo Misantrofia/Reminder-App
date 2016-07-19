daeb1098369ec0c4eda23128b80553208e515370
Catalin David <catalindavid@SL45-205.softvision.ro>
2016-07-19 14:25:50 +0300

    Added CDEditTableViewController in controller and its class Added prototypeCells with their constraints(might suffer additional changes)

---
+++ ReminderApp-OBJC/ReminderApp-OBJC/Classes/Controller/CDEditReminderTableViewController.m
@@ -0,0 +1,13 @@
+//
+//  CDEditReminderTableViewController.m
+//  ReminderApp-OBJC
+//
+//  Created by Catalin David on 19/07/16.
+//  Copyright © 2016 Catalin David. All rights reserved.
+//
+
+#import "CDEditReminderTableViewController.h"
+
+@implementation CDEditReminderTableViewController
+
+@end
