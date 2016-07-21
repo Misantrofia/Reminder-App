53556439dffc643b0205882cb2806ed077e63330
Catalin David <catalindavid@SL45-205.softvision.ro>
2016-07-21 16:16:31 +0300

    - rewritten code, using literals for NSNumber type in the priority enum

--- ReminderApp-OBJC/ReminderApp-OBJC/Classes/ReminderCell/CDEditPriorityCell.m
+++ ReminderApp-OBJC/ReminderApp-OBJC/Classes/ReminderCell/CDEditPriorityCell.m
@@ -26,16 +26,16 @@
 	
 	switch (self.prioritySegmentedControl.selectedSegmentIndex) {
 		case CDPriorityLow:
-			self.reminder.priority = [NSNumber numberWithInteger:CDPriorityLow];
+			self.reminder.priority = @(CDPriorityLow);
 			break;
 		case CDPriorityMedium:
-			self.reminder.priority = [NSNumber numberWithInteger:CDPriorityMedium];
+			self.reminder.priority = @(CDPriorityMedium);
 			break;
 		case CDPriorityHigh:
-			self.reminder.priority = [NSNumber numberWithInteger:CDPriorityHigh];
+			self.reminder.priority = @(CDPriorityHigh);
 			break;
 		case CDPriorityCritical:
-			self.reminder.priority = [NSNumber numberWithInteger:CDPriorityCritical];
+			self.reminder.priority = @(CDPriorityCritical);
 			break;
 		default:
 			break;
